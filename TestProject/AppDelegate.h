0b079c1a3ccdc5ef0ed82ce700262ebfad583026
Ibrahim Hassan <ibrahimhassan@ibrahims-macbook-pro.local>
2018-08-04 00:07:49 +0530

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Ibrahim Hassan on 04/08/18.
+//  Copyright © 2018 Ibrahim Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Ibrahim Hassan on 04/08/18.
+//  Copyright © 2018 Ibrahim Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
